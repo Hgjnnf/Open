44e68b41dd63f853316eb8006c5bc199107fac14
Gordon Cheung <gordonhwc1@gmail.com>
2021-06-28 21:22:11 -0700

    Add password hash in model

--- open-backend/app.js
+++ open-backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
 
 const port = process.env.PORT || 8000;
 
