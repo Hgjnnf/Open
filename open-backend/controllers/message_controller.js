691e4b301b31afd1dd7558175939187c344e0559
Dennp333 <77082803+Dennp333@users.noreply.github.com>
2021-07-18 15:00:51 -0400

    Finish message model and start controlller functions

--- open-backend/controllers/message_controller.js
+++ open-backend/controllers/message_controller.js
@@ -1,13 +1,20 @@
 var Message = require('../models/message_model');
 
 // Display all available messages
-exports.message_list = function(req, res) {
-    res.send('Message list not yet implemented');
+// Currently set up to just display all messages in the DB - need to use current user information to complete
+exports.message_list = function(req, res, next) {
+    Message.find({}, 'title', function(err, list_messages) {
+        if (err) {return next(err);}
+        res.render('message_list', {title: 'Your Messages', message_list: list_messages});
+    });
 };
 
 // Display a particular message
-exports.message_read = function(req, res) {
-    res.send('Message reading not yet implemented');
+exports.message_read = function(req, res, next) {
+    Message.findById(req.params.id, function(err, message) {
+        if (err) {return next(err);}
+        res.render('message_read', {title: message.title, content: message.content});
+    });
 };
 
 // Display message write form on GET
@@ -22,6 +29,7 @@ exports.message_write_post = function(req, res) {
 
 // Display message reply form on GET
 // May need a parameter of message_id or sender_id, maybe store in req
+// Replying can only be done when sender id's are added to the message model
 exports.message_reply_get = function(req, res) {
     res.send('Message reply get not yet implemented');
 };
--- open-backend/models/message_model.js
+++ open-backend/models/message_model.js
@@ -3,10 +3,17 @@ var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema({
     title: {type: String, required: [true, 'Please provide a title']},
-    content: {type: String, required: [true, 'Message cannot be empty']}
-    // Below parameters to be added when user model is done and connected to DB
-    //sender_id: user object id
-    //recipients: list of user object id's
+    content: {type: String, required: [true, 'Message cannot be empty']},
+    available_at: {type: Date},
+    sender_id: {type: Schema.Types.ObjectId, ref: 'User'},
+    recipients: [{type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+// Virtual for message URL
+MessageSchema
+.virtual('url')
+.get(function () {
+  return '/' + this._id;
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
