9adae8e325843a30ee3b4a3fd7633f434780e16c
Gordon Cheung <gordonhwc1@gmail.com>
2021-07-25 15:29:45 -0700

    Use user id only

--- open-backend/controllers/private.js
+++ open-backend/controllers/private.js
@@ -2,6 +2,6 @@ exports.getPrivateData = (req, res, next) => {
     res.status(200).json({
         success: true,
         message: "Access Granted",
-        user: req.user
+        userID: req.user._id
     })
 }
