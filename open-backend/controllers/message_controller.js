892cdfe21fe0e30ba696c8f12ff597b518d061f8
Dennp333 <77082803+Dennp333@users.noreply.github.com>
2021-08-08 22:43:37 -0400

    Finish message sending and recieving

--- open-backend/controllers/message_controller.js
+++ open-backend/controllers/message_controller.js
@@ -1,10 +1,11 @@
 var Message = require('../models/message_model');
+var User = require('../models/User');
 
 // Display all available messages
-// Currently set up to just display all messages in the DB - need to use current user information to complete
 exports.message_list = function(req, res, next) {
     Message.find({
-        available_at: {$lte: new Date()}
+        available_at: {$lte: new Date()},
+        recipients: req.user._id
     },  function(err, list_messages) {
         if (err) {return next(err);}
         res.json({message_list: list_messages});
@@ -21,12 +22,13 @@ exports.message_read = function(req, res, next) {
 
 // Handle message write on POST
 // Validation skipped for now
-exports.message_write_post = function(req, res, next) {
+exports.message_write_post = async function(req, res, next) {
     var message = new Message({
         title: req.body.title,
         content: req.body.content,
         available_at: getAvailableDate(),
-        sender_id: req.user._id
+        sender_id: req.user._id,
+        recipients: await getRecipients(req.user.id)
     });
     message.save(function(err) {
         if (err) {return next(err);}
@@ -44,4 +46,15 @@ function getAvailableDate() {
     const date = new Date();
     date.setDate(date.getDate() + 1);
     return date;
-}
+};
+
+async function getRecipients(ownId) {
+    const recipients = await User.aggregate([{$sample: {size: 3}}]);
+    ids = [];
+    recipients.forEach(function(user) {
+        if (user._id != ownId) {
+            ids.push(user._id);
+        }
+    });
+    return ids;
+};
