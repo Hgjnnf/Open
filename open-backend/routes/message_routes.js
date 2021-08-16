ede78014cf8bca638ced6d8bd8a1c6b6f9657bca
Dennp333 <77082803+Dennp333@users.noreply.github.com>
2021-08-15 20:56:19 -0400

    Add some fixes

--- open-backend/routes/message_routes.js
+++ open-backend/routes/message_routes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {message_write_post, message_reply_post, message_read, message_list} = require('../controllers/message_controller');
+const message_controller = require('../controllers/message_controller');
 const {protect} = require('../middleware/auth');
 
 // POST request for writing a message
-router.route('/write').post(protect, message_write_post);
+router.route('/write').post(protect, message_controller.message_write_post);
 
 //POST request for replying to a message
-router.route('/:id/reply').post(protect, message_reply_post);
+router.route('/:id/reply').post(protect, message_controller.message_reply_post);
 
 // GET request for reading a message
-router.route('/:id').get(protect, message_read);
+router.route('/:id').get(protect, message_controller.message_read);
 
 // GET request for list of all messages
-router.route('/').get(protect, message_list);
+router.route('/').get(protect, message_controller.message_list);
 
 module.exports = router;
