8bb66b3d5a70dbc0892d346e52537b9e76c6e4ed
Gordon Cheung <gordonhwc1@gmail.com>
2021-08-22 23:32:42 -0700

    Add react-icon & finish basic profile page (static)

--- open-frontend/src/components/profile/Profile.js
+++ open-frontend/src/components/profile/Profile.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './Profile.css';
 import { InputBar } from '../common/Input Bar/InputBar';
 import { Username } from './Username';
+import returnArrow from '../../util/media/returnArrow.png';
 
 export class Profile extends React.Component {
     render() {
         return (
             <div className="Profile">
-                <Username username="Hgjnnf"/>
-                <InputBar title="EMAIL" content="Email@gmail.com" />
-                <InputBar title="PASSWORD" content="xxxxxx" />
+                <a href="#" id="return-link"><img src={returnArrow} alt="Return Arrow" id="return-arrow"/></a>
+                <div className="Profile-Main">
+                    <Username username="Hgjnnf"/>
+                    <InputBar title="EMAIL" content="Email@gmail.com" />
+                    <InputBar title="PASSWORD" content="xxxxxx" />
+                </div>
             </div>
         )
     }
