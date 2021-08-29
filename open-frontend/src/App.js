28a637c3d90cab1bf2966fd5bc4e081174630ffe
Gordon Cheung <gordonhwc1@gmail.com>
2021-08-28 19:26:34 -0700

    Connect getProfileData API from backend to frontend

--- open-frontend/src/App.js
+++ open-frontend/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import { Profile } from './components/profile/Profile';
 
 function App() {
+  let authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMTBhZWM4NjhiNzRiN2ZjODg0ODNhNCIsImlhdCI6MTYzMDIwMzMzMCwiZXhwIjoxNjMwMjAzOTMwfQ.Y30auKcfM_LYTthqB-bvKVBLeSPWcKDCMdEcOk5PisE';
+
   return (
     <div className="App">
-      < Profile />
+      < Profile token={authToken} />
     </div>
   );
 }
--- open-frontend/src/components/profile/Profile.js
+++ open-frontend/src/components/profile/Profile.js
@@ -3,19 +3,74 @@ import './Profile.css';
 import { InputBar } from '../common/Input Bar/InputBar';
 import { FaEdit } from 'react-icons/fa';
 import returnArrow from '../../util/media/returnArrow.png';
+import axios from 'axios';
 
 export class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: 'Loading...',
+            email: 'Loading...',
+        }
+    };
+
+    fetchProfileData() {
+        let authToken = this.props.token;
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${authToken}`
+            }
+        }
+
+        axios.get('/profile', config).then(res => {
+            setTimeout(() => {
+                    this.setState({
+                        username: res.data.username,
+                        email: res.data.email
+                    });
+                }, 1000)
+            }
+        ).catch(err => {
+            setTimeout(() => {
+                    this.setState({
+                        username: err.message,
+                        email: err.message
+                    });
+                }, 1000)
+            }
+        );
+    };
+
     render() {
+        let username = this.state.username;
+        let email = this.state.email;
+
         return (
             <div className="Profile">
                 <a href="#" id="return-link"><img src={returnArrow} alt="Return Arrow" id="return-arrow"/></a>
                 <div className="Profile-Main">
                     <h2 id="profile-title">PROFILE</h2>
-                    <InputBar title="USERNAME" content="Hgjnnf" icon={<FaEdit />} />
-                    <InputBar title="EMAIL" content="Email@gmail.com" />
+                    <InputBar title="USERNAME" content={username} icon={<FaEdit />} />
+                    <InputBar title="EMAIL" content={email} />
                     <p id="change-password">Change Password</p>
                 </div>
             </div>
         )
-    }
+    };
+
+    componentDidMount() {
+        this.fetchProfileData()
+    };
+
+    componentDidUpdate(prevState) {
+        let didUsernameChange = this.state.username !== prevState.username;
+        let didEmailChange = this.state.email !== prevState.email;
+
+        if(didUsernameChange || didEmailChange){
+            this.fetchProfileData()
+        }
+    };
 }
