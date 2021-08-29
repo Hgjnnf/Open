4dbdd5a278d9c3a2775c28f3b146cd417f3e6f25
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2021-08-29 01:03:08 -0700

    Update App.js

--- open-frontend/src/App.js
+++ open-frontend/src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { Profile } from './components/profile/Profile';
 
 function App() {
-  let authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZjI2Mjk4Mjc2NjBhNTZhYzczY2QyZCIsImlhdCI6MTYzMDIyMzk2MywiZXhwIjoxNjMwMjI1NzYzfQ.MoTZH0tsRC2OtItXpi9-PqbYFpkXuv6yHkhkp947Vcg';
-
+  let authToken = '';
   return (
     <div className="App">
       < Profile token={authToken} />
