377da359ec19dce498c350a3d7f3804fffde2443
Dennp333 <77082803+Dennp333@users.noreply.github.com>
2021-09-12 14:55:52 -0400

    Add testing constants

---
+++ open-backend/test/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    mainUser: {
+        email: "mainuser@gmail.com",
+        password: "mainuser"
+    },
+    mainUserReg: {
+        username: "mainuser",
+        email: "mainuser@gmail.com",
+        password: "mainuser"
+    },
+    testUser1: {
+        username: "testuser1",
+        email: "testuser1@gmail.com",
+        password: "testuser1"
+    },
+    testUser2: {
+        username: "testuser2",
+        email: "testuser2@gmail.com",
+        password: "testuser2"
+    },
+    testUser3: {
+        username: "testuser3",
+        email: "testuser3@gmail.com",
+        password: "testuser3"
+    },
+    messageTitle: "test",
+    messageContent: "test",
+    writtenContent: "written",
+    unavailableContent: "unavailable"
+}
--- open-backend/test/test.js
+++ open-backend/test/test.js
@@ -5,9 +5,13 @@ const app = express();
 
 require("./testdb")
 const errorHandler = require('../middleware/error');
+const conf = require('./config');
+const utils = require('./utils');
 
 app.use(express.json());
+app.use(errorHandler);
 app.use('/api/auth', require('../routes/auth'));
 app.use('/profile', require('../routes/profile'));
 app.use('/messages', require('../routes/message_routes'));
 
+// Tests to run
